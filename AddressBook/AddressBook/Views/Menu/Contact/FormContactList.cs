85ad97363cf1fdaaa2842a43ab6abe92921a0d43
TempeBrennan <425751342@qq.com>
2019-10-15 20:39:18 +0800

    add contact list page

--- AddressBook/AddressBook/Views/Menu/Contact/FormContactList.cs
+++ AddressBook/AddressBook/Views/Menu/Contact/FormContactList.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,48 @@ namespace AddressBook
 {
     public partial class FormContactList : Form
     {
+        private SqlDataAdapter _adapter;
+        private DataSet _dataSet;
+
         public FormContactList()
         {
             InitializeComponent();
+        }
+
+        private void Fill()
+        {
+            var sql = $"select Contact.Id,Name,Phone,Email,QQ,GroupName " +
+                $"from Contact,ContactGroup " +
+                $"where Contact.GroupId=ContactGroup.Id ";
+
+            if (this.cboCondition.Text == "姓名")
+            {
+                sql += $"and Name like '%{this.txtSearch.Text}%' ";
+            }
+            else
+            {
+                sql += $"and Phone like '%{this.txtSearch.Text}%' ";
+            }
+            sql += "order by Contact.Id";
+
+            using (var connection = new SqlConnection(DBHelper.ConnectionString))
+            {
+                connection.Open();
+                this._adapter = new SqlDataAdapter(sql, connection);
+                this._dataSet = new DataSet();
+                this._adapter.Fill(this._dataSet);
+                this.dgvContactList.DataSource = this._dataSet.Tables[0];
+            }
+        }
+
+        private void FormContactList_Load(object sender, EventArgs e)
+        {
+            this.Fill();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            this.Fill();
         }
     }
 }
